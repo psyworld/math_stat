15e9cfa2bb9ec1522d354e6cea5450133a3c4740
psyworld <psygalaxia@gmail.com>
2018-02-24 00:20:00 +0300

    added sample mean and sample variance

--- lab1/task1.m
+++ lab1/task1.m
@@ -15,26 +15,36 @@ array2 = sort(dlmread("2.txt"));
 %dlmwrite("3.txt", array3)
 array3 = sort(dlmread("3.txt"));
 
-figure(1)
+figure(1);
 w1 = poligon(array1);
 x1 = unique(array1);
 
-figure(2)
+figure(2);
 w2 = poligon(array2);
 x2 = unique(array2);
 
-figure(3)
+figure(3);
 w3 = poligon(array3);
 x3 = unique(array3);
 
-figure(4)
-wi1 = empirical(x1, w1)
+figure(4);
+wi1 = empirical(x1, w1);
 
-figure(5)
-wi2 = empirical(x2, w2)
+figure(5);
+wi2 = empirical(x2, w2);
 
-figure(6)
-wi3 = empirical(x3, w3)
+figure(6);
+wi3 = empirical(x3, w3);
 
+x_1 = sample_mean(x1, w1);
+x_2 = sample_mean(x2, w2);
+x_3 = sample_mean(x3, w3);
 
+db1 = sample_variance(x1, w1, x_1);
+db2 = sample_variance(x2, w2, x_2);
+db3 = sample_variance(x3, w3, x_3);
+
+sigma_1 = sqrt(db1);
+sigma_2 = sqrt(db2);
+sigma_3 = sqrt(db3);
 
